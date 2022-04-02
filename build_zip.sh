0fc090dd78444b9357936a9cbf14dad86024d748
ariffjenong <arifbuditantodablekk@gmail.com>
2022-04-03 03:33:30 +0700

    edit build_zip.sh

--- build_zip.sh
+++ build_zip.sh
@@ -1,20 +1,51 @@
 #!/bin/bash
-cd /cirrus
 
-com ()
-{
+com () {
     #tar --use-compress-program="pigz -k -$2 " -cf $1.tar.gz $1
     tar "-I zstd -1 -T2" -cf $1.tar.zst $1
 }
 
-time com ccache 1
+get_ccache () {
+  cd /cirrus
+  time com ccache 1
+  time rclone copy ccache.tar.* znxtproject:ccache/$ROM_PROJECT -P
+  time rm ccache.tar.*
+  ls -lh
+}
 
+system () {
+  cd /cirrus/rom/out/target/product/maple_dsds/system/etc
+  ls -lh
+  time com selinux 1
+  time rclone copy selinux.tar.* znxtproject:rom/CherishOS/$ROM_PROJECT/system/etc -P
+}
 
-#echo "$rclone_config" > ~/.config/rclone/rclone.conf
-#curl -s https://api.telegram.org/$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d text="Uploading ccache...."
-time rclone copy ccache.tar.* znxtproject:ccache/$ROM_PROJECT -P
-#cd ~
-#time com rom 1
-time rm ccache.tar.*
-ls -lh
-#curl -s https://api.telegram.org/$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d text="Uploading Success"
+product () {
+  cd /cirrus/rom/out/target/product/maple_dsds/system/product/etc
+  ls -lh
+  time com selinux 1
+  time rclone copy selinux.tar.* znxtproject:rom/CherishOS/$ROM_PROJECT/system/product/etc -P
+}
+
+system-ext () {
+  cd /cirrus/rom/out/target/product/maple_dsds/system/system-ext/etc
+  ls -lh
+  time com selinux 1
+  time rclone copy selinux.tar.* znxtproject:rom/CherishOS/$ROM_PROJECT/system/system-ext/etc -P
+}
+
+root () {
+  cd /cirrus/rom/out/target/product/maple_dsds/root
+  ls -lh
+  time rclone copy sepolicy znxtproject:rom/CherishOS/$ROM_PROJECT/system/ -P
+}
+
+get_selinux () {
+  system
+  product
+  system-ext
+  root
+}
+
+get_selinux
+get_ccache
