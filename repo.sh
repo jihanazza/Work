7b6d3e235e95d6e4638fbf4de0235c5cb64cd675
ariffjenong <arifbuditantodablekk@gmail.com>
2022-04-03 04:36:13 +0700

    get selinux

--- repo.sh
+++ repo.sh
@@ -3,8 +3,8 @@ cd /cirrus/rom
     
 sync () {
     #rm -rf .repo/local_manifests
-    #repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b twelve-one -g default,-mips,-darwin,-notdefault
-    #git clone https://github.com/ariffjenong/local_manifest.git --depth=1 -b $ROM_PROJECT .repo/local_manifests
+    repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b twelve-one -g default,-mips,-darwin,-notdefault
+    git clone https://github.com/ariffjenong/local_manifest.git --depth=1 -b $ROM_PROJECT .repo/local_manifests
     repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j24
 }
 
