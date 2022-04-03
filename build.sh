0da8465734e74f5efbe9b00cb70bb90b44304e1b
ariffjenong <arifbuditantodablekk@gmail.com>
2022-04-03 13:48:49 +0700

    get ccache [skip ci]

--- build.sh
+++ build.sh
@@ -12,17 +12,16 @@
  export BUILD_HOSTNAME=linux
  export BUILD_USERNAME=ArifJeNong
  export TZ=Asia/Jakarta
- #curl -s https://api.telegram.org/$TG_TOKEN/sendMessage -d chat_id=$TG_CHAT_ID -d text="$(echo "${var_cache_report_config}")"
 #make sepolicy -j24
 #make bootimage -j24
 #make init -j24
 #make services
 #make systemimage &
-#mka bacon -j24 & #dont remove that '&'
+mka bacon -j24 & #dont remove that '&'
 #sleep 50m #first running
 #sleep 104m #second running
-#sleep 106m #third running
-#kill %1
+sleep 106m #third running
+kill %1
  
- mka bacon -j24
+ #mka bacon -j24
  ccache -s
