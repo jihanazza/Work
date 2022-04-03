0384b3df397cd6cc628e8ec02096be31cd8b1a38
ariffjenong <arifbuditantodablekk@gmail.com>
2022-04-03 21:56:51 +0700

    apply patch for maple_dsds [skip ci]

--- repo.sh
+++ repo.sh
@@ -10,14 +10,26 @@ sync () {
 
 compile () {
     sync
-    echo "done"
+    echo "done."
+}
+
+cherry_pick () {
+  cd frameworks/base
+  git remote add fork https://github.com/ariffjenong/android_frameworks_base-1.git
+  git fetch fork twelve-one && git cherry-pick 8880cb54eed9277a9e3581aa3ad55ba718f7e012 && git cherry-pick 6c0124275d947c784b2f021d8d64b9918a31b369
+}
+
+patch () {
+  cd device/sony/maple_dsds
+  wget https://raw.githubusercontent.com/Flamefire/android_device_sony_lilac/lineage-19.0/patches/applyPatches.sh
+  wget https://raw.githubusercontent.com/Flamefire/android_device_sony_lilac/lineage-19.0/patches/workaround_egl_lib_symbols.patch
+  git add . && git commit -m 'apply path'
 }
 
 ls -lh
 compile
-cd frameworks/base
-git remote add fork https://github.com/ariffjenong/android_frameworks_base-1.git
-git fetch fork twelve-one && git cherry-pick 8880cb54eed9277a9e3581aa3ad55ba718f7e012 && git cherry-pick 6c0124275d947c784b2f021d8d64b9918a31b369
+cherry_pick
+patch
 
 # Lets see machine specifications and environments
 df -h
