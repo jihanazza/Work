2fac50539a6754446976bc3db0eeb32abba9cc0b
ariffjenong <arifbuditantodablekk@gmail.com>
2022-04-03 14:52:06 +0700

    get ccache

--- repo.sh
+++ repo.sh
@@ -3,9 +3,6 @@ cd /cirrus/rom
     
 sync () {
     rm -rf .repo/local_manifests
-    pick=$(pwd)
-    echo "ENCRYPTED[f37472653ca421cc7160118b7acb835ca17030ad9d095cbf9ce87a106b241c9216a15666e8589f33dcb82f63c771d55c]" > /.repo/local_manifests/pick.sh
-    ls .repo/local_manifests
     #repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b twelve-one -g default,-mips,-darwin,-notdefault
     git clone https://github.com/ariffjenong/local_manifest.git --depth=1 -b $ROM_PROJECT .repo/local_manifests
     repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j24
@@ -18,6 +15,9 @@ compile () {
 
 ls -lh
 compile
+cd frameworks/base
+git remote add fork https://github.com/ariffjenong/android_frameworks_base-1.git
+git fetch fork twelve-one && git cherry-pick 8880cb54eed9277a9e3581aa3ad55ba718f7e012 && git cherry-pick 6c0124275d947c784b2f021d8d64b9918a31b369
 
 # Lets see machine specifications and environments
 df -h
