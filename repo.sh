93391ba8294bac35105db3239484f71e5b951f1a
ariffjenong <arifbuditantodablekk@gmail.com>
2022-04-06 03:59:35 +0700

    test sleep 6m first

--- repo.sh
+++ repo.sh
@@ -2,10 +2,10 @@
 cd /cirrus/rom
     
 sync () {
-    rm -rf .repo/local_manifests
+    #rm -rf .repo/local_manifests
     #repo init --depth=1 --no-repo-verify -u https://github.com/CherishOS/android_manifest.git -b twelve-one -g default,-mips,-darwin,-notdefault
-    git clone https://github.com/ariffjenong/local_manifest.git --depth=1 -b $ROM_PROJECT .repo/local_manifests
-    repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j24
+    #git clone https://github.com/ariffjenong/local_manifest.git --depth=1 -b $ROM_PROJECT .repo/local_manifests
+    repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 }
 
 compile () {
@@ -28,7 +28,7 @@ patch () {
 
 ls -lh
 compile
-patch
+#patch
 
 # Lets see machine specifications and environments
 df -h
