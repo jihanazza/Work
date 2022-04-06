80f9aa3d272e0cd64a8331b41e48550213c3fb26
ariffjenong <arifbuditantodablekk@gmail.com>
2022-04-06 12:09:30 +0700

    get new ccache

--- build.sh
+++ build.sh
@@ -14,11 +14,11 @@
 #make sepolicy -j24
 #make bootimage -j24
 make systemimage &
-#mka bacon -j24 & #dont remove that '&'
+#mka bacon -j30 & #dont remove that '&'
 #sleep 108 #first running
 #sleep 106m #second running
 sleep 104m #third running
 kill %1
  
-mka bacon -j24
+#mka bacon -j24
 ccache -s
