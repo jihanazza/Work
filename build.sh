6b202567daa39eb90f4a6d5e695bfe6ea6e99ead
ariffjenong <arifbuditantodablekk@gmail.com>
2022-04-07 12:37:09 +0700

    cherish: sleep after 107m [skip ci]

--- build.sh
+++ build.sh
@@ -15,9 +15,10 @@
 #make bootimage -j24
 #make systemimage &
 mka bacon -j30 & #dont remove that '&'
-sleep 108m #first running
-#sleep 106m #second running
-#sleep 104m #third running
+#sleep 108m #first running
+sleep 107m #second running
+#sleep 105m #third running
+#sleep 104m #fourth running
 kill %1
  
 #mka bacon -j24
