1241038795738bf050a688c9d95ed9ae86ec09b3
jihanazza <jihanazzahranovarisha@gmail.com>
2022-08-25 16:00:42 +0700

    nad-13

--- repo.sh
+++ repo.sh
@@ -2,11 +2,16 @@
     
 sync () {
     cd ~/rom
-    repo init --depth=1 --no-repo-verify -u ${Nusantara} -b 12.1 -g default,-mips,-darwin,-notdefault
-    rclone copy znxtproject:NusantaraProject/manifest/nusantara.xml .repo/manifests/snippets -P
-    rclone copy znxtproject:NusantaraProject/manifest/local_nad.xml .repo/local_manifests -P
+    repo init --depth=1 --no-repo-verify -u ${Nusantara} -b 13 -g default,-mips,-darwin,-notdefault
+    rclone copy znxtproject:NusantaraProject/manifest/13/nusantara.xml .repo/manifests/snippets -P
+    rclone copy znxtproject:NusantaraProject/manifest/13/local_nad.xml .repo/local_manifests -P
+    rclone copy znxtproject:NusantaraProject/manifest/13/lineage-msm8998-yoshino-maple_dsds_defconfig kernel/sony/msm8998/arch/arm64/configs/lineage-msm8998-yoshino-maple_dsds_defconfig -P
+    rclone copy znxtproject:NusantaraProject/manifest/13/Kbuild kernel/sony/msm8998/drivers/staging/qcacld-3.0/Kbuild -P
     repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
     repo sync kernel/sony/msm8998 device/sony/yoshino-common device/sony/maple_dsds vendor/sony/maple_dsds -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+    cd hardware/nad/interfaces
+    git fetch https://github.com/LineageOS/android_hardware_lineage_interfaces lineage-19.1 && git cherry-pick 21e6c8c09692bb9ae21fdc6e4bc1442f6c4cd5d0
+    cd ../../qcom-caf/common && git fetch https://github.com/ArrowOS/android_hardware_qcom-caf_common arrow-13.0 && git checkout FETCH_HEAD && git switch -c arrow-13.0
 }
 
 build () {
@@ -20,10 +25,11 @@ build () {
      export BUILD_HOSTNAME=znxt
      export BUILD_USERNAME=znxt
      export TZ=Asia/Jakarta
-     export USE_GAPPS=true
+     export SELINUX_IGNORE_NEVERALLOWS=true
+     #export USE_GAPPS=true
      export NAD_BUILD_TYPE=OFFICIAL
      export USE_PIXEL_CHARGING=true
-     lunch nad_maple_dsds-user
+     lunch nad_maple_dsds-userdebug
     #make sepolicy -j24
     #make bootimage -j24
     #make systemimage &
