7d906d2af38cf12b68bc1ce3390b7e2fae104660
jihanazza <jihanazzahranovarisha@gmail.com>
2022-08-25 08:58:37 +0700

    encrypt

--- repo.sh
+++ repo.sh
@@ -3,7 +3,6 @@
 sync () {
     cd ~/rom
     repo init --depth=1 --no-repo-verify -u $Nusantara -g default,-mips,-darwin,-notdefault
-    git clone https://github.com/ariffjenong/local_manifest.git --depth 1 -b nad-12.1 .repo/local_manifests
     rclone copy znxtproject:NusantaraProject/manifest/nusantara.xml .repo/manifests/snippets -P
     rclone copy znxtproject:NusantaraProject/manifest/local_nad.xml .repo/local_manifests -P
     repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
