5ef808053207052737380b89df2d478b0a747ac6
jihanazza <jihanazzahranovarisha@gmail.com>
2022-08-25 08:09:39 +0700

    encrypt

--- retry.sh
+++ retry.sh
@@ -12,12 +12,12 @@ retry_ccache () {
     export CCACHE_EXEC=$(which ccache)
 	hit_rate=$(ccache -s | awk '/hit rate/ {print $4}' | cut -d'.' -f1)
 	if [ $hit_rate -lt 99 ]; then
-		git clone ${TOKEN}/ariffjenong/build -b $ROM_PROJECT clone && cd clone
+		git clone ${TOKEN}/jihanazza/CI-Build -b $ROM_PROJECT clone && cd clone
 		git commit --allow-empty -m "Retry: Ccache loop $(date -u +"%D %T%p %Z")"
 		git push -q
 	else
 		echo "Ccache is fully configured"
-		git clone ${TOKEN}/ariffjenong/build -b $ROM_PROJECT clone && cd clone
+		git clone ${TOKEN}/jihanazza/CI-Build -b $ROM_PROJECT clone && cd clone
 		git commit --allow-empty -m "Retry Build $(date -u +"%D %T%p %Z")"
 		git push -q
 	fi
