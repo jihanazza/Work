867ce617b6d0d35139bc937e549b2d39913f2146
jihanazza <jihanazzahranovarisha@gmail.com>
2022-09-01 10:24:38 +0700

    try

--- repo.sh
+++ repo.sh
@@ -11,7 +11,7 @@ sync () {
     rclone copy znxtproject:NusantaraProject/manifest/13/Kbuild kernel/sony/msm8998/drivers/staging/qcacld-3.0/Kbuild -P
     cd hardware/nad/interfaces
     git fetch https://github.com/LineageOS/android_hardware_lineage_interfaces lineage-19.1 && git cherry-pick 21e6c8c09692bb9ae21fdc6e4bc1442f6c4cd5d0
-    cd ../../qcom-caf/common && git fetch https://github.com/ArrowOS/android_hardware_qcom-caf_common arrow-13.0 && git checkout FETCH_HEAD && git switch -c arrow-13.0
+    cd ../../qcom-caf/common && git fetch https://github.com/ArrowOS/android_hardware_qcom-caf_common arrow-13.0 && git merge FETCH_HEAD
 }
 
 build () {
