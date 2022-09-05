ebb9af9736a84745eb6ab794929006636a136ee6
jihanazza <jihanazzahranovarisha@gmail.com>
2022-09-05 20:32:04 +0700

    boot.img

--- repo.sh
+++ repo.sh
@@ -2,14 +2,10 @@
     
 sync () {
     cd ~/rom
-    repo init --depth=1 --no-repo-verify -u ${Nusantara} -b 13 -g default,-mips,-darwin,-notdefault
-    rclone copy znxtproject:NusantaraProject/manifest/13/nusantara.xml .repo/manifests/snippets -P
-    rclone copy znxtproject:NusantaraProject/manifest/13/local_nad.xml .repo/local_manifests -P
+    repo init --depth=1 --no-repo-verify -u ${Nusantara} -b 12.1 -g default,-mips,-darwin,-notdefault
+    rclone copy znxtproject:NusantaraProject/manifest/nusantara.xml .repo/manifests/snippets -P
+    rclone copy znxtproject:NusantaraProject/manifest/local_nad.xml .repo/local_manifests -P
     repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-    repo sync kernel/sony/msm8998 device/sony/yoshino-common device/sony/maple_dsds vendor/sony/maple_dsds -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
-    cd hardware/nad/interfaces
-    git fetch https://github.com/LineageOS/android_hardware_lineage_interfaces lineage-19.1 && git cherry-pick 21e6c8c09692bb9ae21fdc6e4bc1442f6c4cd5d0
-    cd ../../qcom-caf/common && git fetch https://github.com/ArrowOS/android_hardware_qcom-caf_common arrow-12.1 && git checkout FETCH_HEAD
 }
 
 build () {
@@ -23,17 +19,17 @@ build () {
      export BUILD_HOSTNAME=znxt
      export BUILD_USERNAME=znxt
      export TZ=Asia/Jakarta
-     export SELINUX_IGNORE_NEVERALLOWS=true
-     export ALLOW_MISSING_DEPENDENCIES=true
+     #export SELINUX_IGNORE_NEVERALLOWS=true
+     #export ALLOW_MISSING_DEPENDENCIES=true
      export USE_GAPPS=true
      export NAD_BUILD_TYPE=OFFICIAL
      export USE_PIXEL_CHARGING=true
-     lunch nad_maple_dsds-userdebug
+     lunch nad_maple_dsds-user
     #make sepolicy -j24
-    #make bootimage -j24
+    make bootimage -j30
     #make systemimage &
     #make installclean
-    mka nad -j30
+    #mka nad -j30
 }
 
 compile () {
@@ -64,9 +60,9 @@ push_vendor () {
 
 cd ~/rom
 ls -lh
-compile &
-sleep 114m
-kill %1
+compile #&
+#sleep 114m
+#kill %1
 #push_kernel
 #push_device
 #push_yoshino
